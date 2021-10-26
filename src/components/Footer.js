96331d837b72307858d56d9872f32452beede280
Andre Stackhouse <stack@pop-os.localdomain>
2021-10-26 07:52:13 -0700

    update components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,15 @@
 import * as React from "react"
-import {
-  centerContainer,
-} from '../styles/global.module.scss'
 
 const Footer = () =>
   <footer>
-    <div className={centerContainer}>
-      <span>© 2021 by EDGI</span>
-      <ul>
-        <li><a href='https://twitter.com/EEW_Network'>Twitter</a></li>
-        <li><a href='https://www.facebook.com/envirodgi'>Facebook</a></li>
-        <li><a href='https://github.com/edgi-govdata-archiving/Environmental-Enforcement-Watch'>Github</a></li>
-        <li><a href='mailto:info@environmentalenforcementwatch.org'>Email</a></li>
-      </ul>
+    <div className='container'>
+      <div>
+        <a href='https://twitter.com/EEW_Network'>Twitter</a>
+        <a href='https://www.facebook.com/envirodgi'>Facebook</a>
+        <a href='https://github.com/edgi-govdata-archiving/Environmental-Enforcement-Watch'>Github</a>
+        <a href='mailto:info@environmentalenforcementwatch.org'>Email</a>
+      </div>
+      <span>© {new Date().getFullYear()} by EDGI</span>
     </div>
   </footer>
 
--- src/components/HeaderItem.js
+++ src/components/HeaderItem.js
@@ -1,15 +1,14 @@
 import * as React from "react"
 import { Link } from 'gatsby'
-import { active, headerItem, headerSublink } from '../styles/global.module.scss'
 
 const HeaderItem = ({link, activeLink}) =>
-  <div className={headerItem}>
-    <Link key={link.url} to={link.url} className={activeLink === link.text ? active : "inactive"}>
+  <div className='headerItem'>
+    <Link key={link.url} to={link.url} className={activeLink === link.text ? 'active' : "inactive"}>
       {link.text}
     </Link>
     
     {
-      link.sublinks ? <ul>{link.sublinks.map(sublink => <Link to={sublink.url}><li className={headerSublink}>{sublink.text}</li></Link>)}</ul> : null
+      link.sublinks ? <ul>{link.sublinks.map(sublink => <Link to={sublink.url}><li className='headerSublink'>{sublink.text}</li></Link>)}</ul> : null
     }
     
   </div>

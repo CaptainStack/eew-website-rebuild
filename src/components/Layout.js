142dc1d84d3787d3ab9cc1255298418edd6ef63e
CaptainStack <captainstack@gmail.com>
2021-10-30 14:46:27 -0700

    Update Layout to use new Head component w/ ReactHelmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Header from '../components/Header'
+import Head from '../components/Head'
 // import Notification from '../components/Notification'
 import Sidebar from '../components/Sidebar'
 import Footer from '../components/Footer'
@@ -7,11 +8,7 @@ import '../styles/styles.scss'
 
 const Layout = ({pageTitle, children, sidebarLinks, activeHeaderLink}) => 
   <div className={`Layout ${pageTitle}`}>
-    {/* Metadata */}
-    <title>{pageTitle} | Environmental Enforcement Watch</title>
-    <meta content='https://environmentalenforcementwatch.org/static/1c753d31dbdd94ab2b736e4da6ae656a/0a47e/eew-map.png' property="og:image" />
-    <meta property="og:description" content="A data accessibilty project from the Environmental Data Governance Initiative" />
-    
+    <Head pageTitle={pageTitle} />
     <Header activeLink={activeHeaderLink}/>
     {/* <Notification /> */}
     <div className='middle'>

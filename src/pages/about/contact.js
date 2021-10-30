9a27366135163417696151e0321d1aca90f6ae3c
CaptainStack <captainstack@gmail.com>
2021-10-30 13:10:13 -0700

    Clean up contact form

--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -5,21 +5,28 @@ import { AboutSidebarLinks } from '../../helpers/constants'
 
 const ContactPage = () => 
   <Layout pageTitle="Contact" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
-    <p><strong>Press Contact: <a href='environmentalenforcementwatch@gmail.com'>environmentalenforcementwatch@gmail.com</a></strong></p>
+    {/* <p><strong>Press Contact: <a href='environmentalenforcementwatch@gmail.com'>environmentalenforcementwatch@gmail.com</a></strong></p> */}
     <h1>Contact</h1>
       <form action="https://submit-form.com/y28zl2Mj">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" placeholder="Name" required="" />
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="Email" required="" />
-          <label htmlFor="message">Message</label>
-          <textarea
-              id="message"
-              name="message"
-              placeholder="Message"
-              required=""
-          ></textarea>
-          <button type="submit">Send</button>
+      <fieldset>
+        <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" required="" />
+        </div>
+        <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" required="" />
+        </div>
+        <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          required="">
+        </textarea>
+        <button className='button' type="submit">Send</button>
+        </div>
+        </fieldset>
       </form>
   </Layout>
 

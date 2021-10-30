55d77da4a552d8c59d21d6c89d2f75b201c51795
CaptainStack <captainstack@gmail.com>
2021-10-30 14:54:05 -0700

    Add meta description tag for SEO

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,6 +7,7 @@ const Head = ({pageTitle}) =>
     <title>{pageTitle} | Environmental Enforcement Watch</title>
     <meta content='https://environmentalenforcementwatch.org/static/1c753d31dbdd94ab2b736e4da6ae656a/0a47e/eew-map.png' property="og:image" />
     <meta property="og:description" content="A data accessibilty project from the Environmental Data Governance Initiative" />
+    <meta name="description" content="A data accessibilty project from the Environmental Data Governance Initiative" />
   </Helmet>
 
 export default Head

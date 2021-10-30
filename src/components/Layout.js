2110276259cebfd6975195941351d1de000e54ff
CaptainStack <captainstack@gmail.com>
2021-10-30 13:40:53 -0700

    Add description metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,6 +10,7 @@ const Layout = ({pageTitle, children, sidebarLinks, activeHeaderLink}) =>
     {/* Metadata */}
     <title>{pageTitle} | Environmental Enforcement Watch</title>
     <meta content='https://environmentalenforcementwatch.org/static/1c753d31dbdd94ab2b736e4da6ae656a/0a47e/eew-map.png' property="og:image" />
+    <meta property="og:description" content="A data accessibilty project from the Environmental Data Governance Initiative" />
     
     <Header activeLink={activeHeaderLink}/>
     {/* <Notification /> */}

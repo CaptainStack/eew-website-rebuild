c4cf501033841b4e6580522a6bac908403525a9f
CaptainStack <captainstack@gmail.com>
2021-10-30 14:45:47 -0700

    Create new Head component to hold page metadata

---
+++ src/components/Head.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Helmet } from 'react-helmet';
+
+const Head = ({pageTitle}) =>
+  <Helmet>
+    <html lang="en" />
+    <title>{pageTitle} | Environmental Enforcement Watch</title>
+    <meta content='https://environmentalenforcementwatch.org/static/1c753d31dbdd94ab2b736e4da6ae656a/0a47e/eew-map.png' property="og:image" />
+    <meta property="og:description" content="A data accessibilty project from the Environmental Data Governance Initiative" />
+  </Helmet>
+
+export default Head

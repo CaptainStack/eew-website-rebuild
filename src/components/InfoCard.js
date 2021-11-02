2ac1f282447a6c7e50661a29c6b8fef9935be452
Andre Stackhouse <captainstack@gmail.com>
2021-11-01 17:53:43 -0700

    Check in updated styles for InfoCard.

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,13 +1,19 @@
 import * as React from "react"
-import Map from "../images/eew-map.png"
 
 const InfoCard = ({title, link, image, description}) =>
-  <a href='https://www.google.com' target='_blank' rel='noopener'>
-    <div className='InfoCard'>
-        <h1>{title}</h1>
-        {image ? <img src={Map} /> : null}
+  link ? 
+  <a href={link} className='InfoCard' target='_blank' rel='noreferrer noopener' >
+    <div>
+        <strong>{title}</strong>
+        {image ? <img src={image} alt={description}/> : null}
         <p>{description}</p>
     </div>
-  </a>
-
+  </a> :
+  <span className='InfoCard'>
+    <div>
+      {title ? <strong>{title}</strong> : null}
+      {image ? <img src={Map} alt={description}/> : null}
+      {description ? <p>{description}</p> : null}
+    </div>
+  </span>
 export default InfoCard

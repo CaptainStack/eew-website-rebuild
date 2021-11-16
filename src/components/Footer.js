4c640d2ca9d628e7f89a12dd58607e2bfad462ed
CaptainStack <captainstack@gmail.com>
2021-11-16 10:11:09 -0800

    Sync footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import EDGILogo from "../images/EDGI-logo.png"
 
 const Footer = () =>
   <footer>
@@ -9,6 +10,7 @@ const Footer = () =>
         <a href='https://github.com/edgi-govdata-archiving/Environmental-Enforcement-Watch'>Github</a>
         <a href='mailto:info@environmentalenforcementwatch.org'>Email</a>
       </div>
+      <a href='https://envirodatagov.org/' target='_blank' rel='noopener noreferrer'><img src={EDGILogo} /></a>
       <span>© Environmental Data Governance Initiative (EDGI) {new Date().getFullYear()}.</span>
       <span>EDGI is a project of Multiplier, a tax-exempt nonprofit 501(c)3 umbrella organization (Tax ID 91-2166435).</span>
     </div>
